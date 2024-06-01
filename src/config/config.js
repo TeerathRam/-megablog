90ba53c0b6bb387b5260dad7fd03f6f5c89971b8
Teerath Ram <teerathjammar8@gmail.com>
2024-06-01 22:38:30 +0530

    add user controller

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    databaseId: String(import.meta.env.VITE_APPWRITE_BATABASE_ID),
-    collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_BATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
 
