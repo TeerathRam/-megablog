f6f8c1167780ff282ac95c75575ce6eb0f1d8117
Teerath Ram <teerathjammar8@gmail.com>
2024-06-03 10:25:31 +0530

    add file controller

---
+++ src/appwrite/conf.js
@@ -0,0 +1,160 @@
+import config from "../config/config";
+import { Client, ID, Databases, Storage, Query } from "appwrite"; 
+
+export class Service{
+    client = new Client()
+    databases;
+    bucket;
+
+    constructor () {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+            this.databases = new Databases(this.client)
+            this.bucket = new Storage(this.client)
+
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title, 
+                    content, 
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+        // have a bug which i have to fix later
+    async getPosts(queries= [Query.equal("status",["active"])]) {
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries
+            )       
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    // file upload service
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error)
+            return false
+        }
+    } 
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error)
+            return false
+        }
+    }
+
+    getFilePreview(fileId) {
+        try {
+            return this.bucket.getFilePreview(
+                config.appwriteBucketId,
+                fileId   
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error)
+            return false
+        }
+    }
+
+}
+
+const service = new Service()
+
+export default service
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
