ca8c5fbb57aafc0015cfd8f34a320a179738f1b1
Teerath Ram <teerathjammar8@gmail.com>
2024-06-06 08:39:14 +0530

    configure routes

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import AddPost from "./AddPost"
+import AllPost from "./AllPost"
+import EditPost from "./EditPost"
+import Home from "./Home"
+import Login from "./Login"
+import Post from "./Post"
+import Signup from "./Signup"
+
+export {
+    AddPost,
+    AllPost,
+    EditPost,
+    Home,
+    Login,
+    Post,
+    Signup
+}
